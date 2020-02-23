a37d7e92620146567515c098ff6bbf93c05072a7
AFrukacz <afrukacz@gmail.com>
2020-02-23 21:41:43 +0000

    Round 2 Edits

--- app.rb
+++ app.rb
@@ -9,9 +9,6 @@ before { puts "Parameters: #{params}" }
 # enter your Dark Sky API key here
 ForecastIO.api_key = "be89e112d21cf02eb80269c2d6cb7269"
 
-# News API
-url = "https://newsapi.org/v2/top-headlines?country=us&apiKey=13454b5bbb1440d3b6a79ad60bf2a395"
-news = HTTParty.get(url).parsed_response.to_hash
 
 get "/" do
 # show a view that asks for the location
@@ -20,25 +17,25 @@ end
 
 get "/news" do
   # do everything else
-results = Geocoder.search(params["q"])
+    results = Geocoder.search(params["q"])
     lat_long = results.first.coordinates # => [lat, long]
     location = results.first.city
-    "#{lat_long[0]} #{lat_long[1]}"
+    lat = "#{lat_long [0]}"
+    long = "#{lat_long [1]}"
+    url = "https://newsapi.org/v2/top-headlines?country=us&apiKey=13454b5bbb1440d3b6a79ad60bf2a395"
+    news = HTTParty.get(url).parsed_response.to_hash
     view "news"
 
-    # Define the lat and long
-@lat = "#{lat_long [0]}"
-@long = "#{lat_long [1]}"
 
+# # display current weather
+# puts "In #{@location}, it is currently #{@current_temperature} and #{@current_conditions}."
 
-# display current weather
-puts "In #{@location}, it is currently #{@current_temperature} and #{@current_conditions}."
+# # weather forecast
+# day_hightemp = []
+# day_condition = []
 
-# weather forecase
-day_hightemp = []
-day_condition = []
-
-for day in forecast["daily"]{"data"}
-    day_hightemp << "#{day["temperatureHigh"]}"
-    day_condition << "#{day["summary"]}"
+# for day in forecast["daily"]{"data"}
+#     day_hightemp << "#{day["temperatureHigh"]}"
+#     day_condition << "#{day["summary"]}"
+# end    
 end
